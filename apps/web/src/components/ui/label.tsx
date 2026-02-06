e8ea8169d61ffb841aeb5ad9ce5c726959fa722f
sodanstjrwns-max <genspark_dev@genspark.ai>
2026-02-06 08:50:46 +0000

    fix: Label 컴포넌트 추가

---
+++ apps/web/src/components/ui/label.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <label
+        ref={ref}
+        className={cn(
+          'text-sm font-medium text-gray-700 block mb-1.5',
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+Label.displayName = 'Label';
+
+export { Label };
